65c06d7999c0a7479c1027b4b032049a7c32b54f
shenxin <wb_shenxin@meituan.com>
2022-08-28 00:01:11 +0800

    注册界面 html

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
-
+import { MantineProvider } from "@mantine/core";
 import "./App.css";
 import { SignUpPage } from "./views/SignInPage";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/signup" element={<SignUpPage />} />
-      </Routes>
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <div className="App">
+        <Routes>
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </div>
+    </MantineProvider>
   );
 }
 
--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import { Button, PasswordInput } from "@mantine/core";
+import { TextInput } from "@mantine/core";
+import { IconAt } from "@tabler/icons";
 export const SignUpPage: React.FC = () => {
   return (
     <div>
-      <h1>Sign Up</h1>
+      <h1>番茄土豆注册</h1>
+      <TextInput icon={<IconAt />} placeholder="请输入用户名" />
+      <PasswordInput placeholder="密码" />
+      <PasswordInput placeholder="确认密码" />
+      <Button>Click me!</Button>
     </div>
   );
 };
